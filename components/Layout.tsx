248ffcfa5f6717918eb0e31788802defedc9acde
Tomas Jansson <tomas.jansson@gmail.com>
2022-04-16 22:10:10 +0200

    Add some fonts and menu

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -12,6 +12,7 @@ import {
     HiX,
     HiOutlineSearch,
 } from "react-icons/hi";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 export interface LayoutProps {
     title: string;
@@ -22,17 +23,32 @@ export default function Layout({ children, title }: LayoutProps) {
 
     return (
         <>
-            <div className="flex overflow-hidden h-screen">
-                <aside className="flex-row w-48">
-                    <div className="h-16 bg-red-600">Automation Demo</div>
-                    <nav className="bg-blue-400 flex-1 h-full space-y-2 pt-2 px-2">
-                        <a className="flex py-2 px-4 rounded-md hover:bg-blue-700" href="#">Resource group</a>
-                        <a className="flex py-2 px-4 rounded-md hover:bg-blue-700" href="#">Blob storage</a>
+            <div className="flex-row sm:flex overflow-hidden h-screen">
+                <aside className="flex-row w-full sm:max-w-xs">
+                    <div className="h-16 bg-red-600 px-4 flex items-center gap-2">
+                        <button onClick={() => setSidebarOpen(s => !s)} className="sm:hidden">
+                            <GiHamburgerMenu size={32} />
+                        </button>
+                        <h1 className="text-2xl">
+                            Automation Demo
+                        </h1>
+                    </div>
+                    {
+                        sidebarOpen && (
+                            <nav className="bg-blue-400 flex-1 h-full space-y-2 px-2 py-2 sm:hidden">
+                                <a className="flex py-2 px-2 rounded-md hover:bg-blue-700" href="#">Resource group</a>
+                                <a className="flex py-2 px-2 rounded-md hover:bg-blue-700" href="#">Repository</a>
+                            </nav>
+                        )
+                    }
+                    <nav className="bg-blue-400 flex-1 h-full space-y-2 py-2 px-2 hidden sm:block">
+                        <a className="flex py-2 px-2 rounded-md hover:bg-blue-700" href="#">Resource group</a>
+                        <a className="flex py-2 px-2 rounded-md hover:bg-blue-700" href="#">Repository</a>
                     </nav>
                 </aside>
                 <main className="flex-row flex-1">
                     <header className="h-16 bg-red-600 z-50 p-4">
-                        <h1 className="font-bold text-2xl">{title}</h1>
+                        <h2 className="font-bold text-2xl">{title}</h2>
                     </header>
                     <div className="flex-1 h-screen overflow-hidden -mt-12 pt-12">
                         <div className="overflow-y-auto h-full bg-green-400">
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  return (
-    <Html>
-      <Head />
-      <body className="overflow-hidden h-screen">
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+    return (
+        <Html>
+            <Head>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+                <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap" rel="stylesheet" />
+            </Head>
+            <body className="overflow-hidden h-screen">
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
 }
